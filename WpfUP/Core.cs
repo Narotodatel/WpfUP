90feed4c09b16171dd78c646ef28d1aed0ac2952
235005 <235005@KB37-303-C04>
2026-05-25 11:32:14 +0300

    Подключил бд + класс Core

---
+++ WpfUP/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUP
+{
+    internal class Core
+    {
+        public static BookPlatformDBEntities Context = new BookPlatformDBEntities();
+    }
+}
